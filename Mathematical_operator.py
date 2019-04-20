1bbdc1273de95f2c36ef69875286d46198eda92c
Itis-ravi <49616082+Itis-ravi@users.noreply.github.com>
2019-04-20 23:46:12 +0530

    Create Mathematical_operator.py

---
+++ Mathematical_operator.py
@@ -0,0 +1,24 @@
+# Mathematical operator in python
+
+# Addition
+print("Mathematical operator + (addition) ")
+print(5 + 2)
+
+# substraction
+print("Mathematical operator - (substraction) ")
+print(5 - 2)
+
+# multiplication
+print("Mathematical operator * (multiplication) ")
+print(5 * 2)
+
+# Division
+print("Mathematical operator / (division) This will return quotient as it is ")
+print(5/2)
+
+print("Mathematical operator // (floor division) This will also return quotient, however ony the integer part, "
+      "the fractional part is eliminated  ")
+print(5 // 2)
+
+print("Mathematical operator % (modulus) This returns remainder from division ")
+print(5 % 2)
