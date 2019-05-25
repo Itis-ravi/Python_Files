fc5f08c87b81d6b0717229a953ebf796cc2dcdd9
It's_ravi <49616082+Itis-ravi@users.noreply.github.com>
2019-05-25 23:51:47 +0530

    Create For_loop.py

---
+++ For_loop.py
@@ -0,0 +1,18 @@
+x = ['Python', 'Java','c++','C#']
+
+print('printing contents of list')
+for i in x:
+    print(i)
+    
+print()
+
+print('printing values in range of 1 to 11')
+for r in range(1,11):
+    print(r)
+
+print()
+
+print ('Loop with if  condition')
+for y in range(1,21):
+    if y%5 == 0 :
+        print(y)
