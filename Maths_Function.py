b7d8d39ac44ef470dc1944ef4b433fb0dd4a42c4
It's_ravi <49616082+Itis-ravi@users.noreply.github.com>
2019-05-25 23:47:53 +0530

    Create Maths_Function.py

---
+++ Maths_Function.py
@@ -0,0 +1,22 @@
+import math
+
+# variable
+x, y = 5, 7
+
+# Square root
+print(math.sqrt(x))
+
+# Floor
+print(math.floor(x))
+
+# ceiling
+print(math.ceil(x))
+
+# Power
+print(math.pow(x,3))
+
+# PI
+print(math.pi)
+
+# factorial
+print(math.factorial(x))
