efe9877a9e83b5d95179a673c3ee8cbac77f7a0a
It's_ravi <49616082+Itis-ravi@users.noreply.github.com>
2019-05-25 23:49:40 +0530

    Create conditional_statement.py

---
+++ conditional_statement.py
@@ -0,0 +1,14 @@
+# If elif and else
+# legacy example to test odd and even number
+x = int(input('Enter a Number to Begin \n'))
+if x % 2 == 0:
+    print('Even')
+else:
+    print('odd')
+
+if (x==1):
+    print('One')
+elif (x>=2):
+    print('Number seems to be greater than two')
+else:
+    print('Bye')
